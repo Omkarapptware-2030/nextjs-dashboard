30ec2134f5cf81b3b26689861f37b486e4ad90fb
Omkar <omkar.jadhav@whitecode.in>
2025-01-29 17:38:50 +0530

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <p>Invoices Dashboard</p>;
+};
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p className="text-2xl m-5">first page</p>;
+}
